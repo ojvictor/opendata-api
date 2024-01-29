07a1f4f41b20b3451e2935bb4cf41ebcedb8c43d
João Bárbara <jvictor32@gmail.com>
2024-01-29 11:08:23 -0300

    Changing Analysis serialize result

--- app/models/analysis.rb
+++ app/models/analysis.rb
@@ -1,5 +1,4 @@
 class Analysis < ApplicationRecord
-
   paginates_per 25
 
   belongs_to :page
@@ -7,10 +6,14 @@ class Analysis < ApplicationRecord
   belongs_to :open_gov_principle
 
   def as_json(options={})
-    h = super(
-      include: [:page, :open_gov_principle, :dw_best_pratice]
-    )
-    h[:analysis_date] = (I18n.l(self.analysis_date) unless self.analysis_date.blank?)
-    h
+    super(options.merge(
+      root: true,
+      include: {
+        page: { only: %i[id] },
+        open_gov_principle: { only: %i[id] },
+        dw_best_pratice: { only: %i[id] }
+      },
+      except: %i[page_id open_gov_principle_id dw_best_pratice_id]
+    ))
   end
 end
